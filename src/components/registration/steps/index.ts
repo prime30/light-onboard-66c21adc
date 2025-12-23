17f0c4a350fed601d096dd8b75786a60893419aa
Jason Lemrond <jason.lemrond@icloud.com>
2025-12-23 10:30:23 -0500

    Remove dead steps and registration components

--- src/components/registration/steps/index.ts
+++ src/components/registration/steps/index.ts
@@ -1,6 +1,5 @@
 // Steps barrel exports
 export { AccountTypeForm } from "./AccountTypeForm";
-export { AccountTypeStep } from "./AccountTypeStep";
 export { BusinessLocationStep, countries, provinces, states } from "./BusinessLocationStep";
 export { BusinessOperationStep } from "./BusinessOperationStep";
 export {
@@ -10,14 +9,11 @@ export {
   isValidPhoneNumber,
 } from "./ContactBasicsStep";
 export { LicenseStep } from "./LicenseStep";
-export { LoginStep } from "./LoginStep";
 export { OnboardingForm } from "./OnboardingForm";
-export { PersonalInfoStep } from "./PersonalInfoStep";
 export { PreferencesStep } from "./PreferencesStep";
 export { SchoolInfoStep } from "./SchoolInfoStep";
 export { SignInForm } from "./SignInForm";
 export { SuccessForm } from "./SuccessForm";
-export { SuccessStep } from "./SuccessStep";
 export { SummaryForm } from "./SummaryForm";
 export { TaxExemptionStep } from "./TaxExemptionStep";
 export { WholesaleTermsStep } from "./WholesaleTermsStep";
